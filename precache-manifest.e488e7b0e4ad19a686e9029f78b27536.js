1eb8c745bf4da46cfae50e071021fecd4e7c9bae
RomanKazantsev13 <dj.ka3anova@gmail.com>
2022-01-25 15:58:09 +0300

    Updates

---
+++ precache-manifest.e488e7b0e4ad19a686e9029f78b27536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b439c27b948b6c4d9a7",
+    "url": "/card-maker/static/css/main.f9abb561.chunk.css"
+  },
+  {
+    "revision": "6b439c27b948b6c4d9a7",
+    "url": "/card-maker/static/js/main.5d35fe83.chunk.js"
+  },
+  {
+    "revision": "ca996e8e03d04bd57217",
+    "url": "/card-maker/static/js/runtime~main.45b245e7.js"
+  },
+  {
+    "revision": "885900ac5462aa640395",
+    "url": "/card-maker/static/js/2.e7ea5e0e.chunk.js"
+  },
+  {
+    "revision": "2ffa3ba91de92ce67e204f7794823776",
+    "url": "/card-maker/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/card-maker/precache-manifest.e488e7b0e4ad19a686e9029f78b27536.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/card-maker/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
